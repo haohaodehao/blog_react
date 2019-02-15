0de9e6eb3ee457f8bb9946e38c87b0774dcfcfad
wanghao <wanghao@titgroup.cn>
2019-02-15 14:37:31 +0800

    finish a base blog system

--- imports/ui/Blog.jsx
+++ imports/ui/Blog.jsx
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { withTracker } from 'meteor/react-meteor-data';
 import Blogs from '../api/blogs';
 
 class Blog extends Component {
+    handleSubmit(event) {
+        event.preventDefault();
+
+        // Find the text field via the React ref
+        const name = ReactDOM.findDOMNode(this.refs.textInputName).value.trim();
+        const description = ReactDOM.findDOMNode(this.refs.textInputDescription).value.trim();
+
+        Blogs.insert({
+            name,
+            description,
+            createdAt: new Date(), // current time
+        });
+
+        // Clear form
+        ReactDOM.findDOMNode(this.refs.textInputName).value = '';
+        ReactDOM.findDOMNode(this.refs.textInputDescription).value = '';
+    }
+
     // state = {
     //   counter: 0,
     // }
@@ -19,7 +38,26 @@ class Blog extends Component {
         );
         return (
             <div>
-                <p>Read My blogs!</p>
+
+                <form className="new-blogs" onSubmit={this.handleSubmit.bind(this)} >
+                    <div>
+                        <input
+                            type="text"
+                            ref="textInputName"
+                            placeholder="Type to add new name"
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            ref="textInputDescription"
+                            placeholder="Type to add new description"
+                        />
+                    </div>
+                    <input type="submit" value="Submit" />
+                </form>
+
+                <h5>Read My blogs!</h5>
                 <div>{ blogs }</div>
             </div>
         );
@@ -28,9 +66,9 @@ class Blog extends Component {
     makeBlog(blog) {
         return (
             <div key={blog._id}>
-                <p>
+                <h5>
                     {blog.name}
-                </p>
+                </h5>
                 <p>
                     {blog.description}
                 </p>
